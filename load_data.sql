fb7843a316fdbeb63e6e0559f07b03b8fd5fc926
Bobby Henson <hunnant@Bobbys-MacBook-Pro.local>
2017-08-05 12:14:10 -0500

    load_data.sql

--- load_data.sql
+++ load_data.sql
@@ -1,7 +1,35 @@
 /*
-    INSERT INTO Major(majorId, majorName, majorDescription, department) 
+    INSERT INTO Major(majorId, majorName, majorDescription, department)
     VALUES(1, 'Accounting', 'This is the nerds', 'Business');
 
     INSERT INTO Customer(accountNum, firstName, lastName, phoneNum, billingAddress)
-        Values(1, 'Bobby', 'Henson', 8174567899, '123 Gdcd Blvd, Grand Prairie, TX');
+        Values(1, 'Jane', 'Doe', 3214567899, '123 Gdcd Blvd, Grand Prairie, TX');
+
+    INSERT INTO Customer(accountNum, firstName, lastName, phoneNum, billingAddress)
+        Values(2, 'John', 'Smith', 8174257659, '813 King Drive, Fort Worth, TX');
+
+    INSERT INTO Customer(accountNum, firstName, lastName, phoneNum, billingAddress)
+        Values(3, 'Rob', 'King', 6915432676, '555 Gold Dig St, Colony, TX');
+
+    INSERT INTO Customer(accountNum, firstName, lastName, phoneNum, billingAddress)
+        Values(4, 'Nate', 'Ko', 7658971234, '333 Panera Dr, Arlington, TX');
+
+    INSERT INTO Customer(accountNum, firstName, lastName, phoneNum, billingAddress)
+        Values(5, 'Alex', 'Right', 7644569191, '321 Vergo Dr, Austin, TX');
+
+    INSERT INTO Customer(accountNum, firstName, lastName, phoneNum, billingAddress)
+        Values(6, 'lauren', 'Lee', 1236540987, '89 Wee Wee Blvd, Cedar Hill, TX');
+
+    INSERT INTO Customer(accountNum, firstName, lastName, phoneNum, billingAddress)
+        Values(7, 'Jacky', 'Nice', 6789909321, '99 Old T St, Carrollton, TX');
+
+    INSERT INTO Customer(accountNum, firstName, lastName, phoneNum, billingAddress)
+        Values(8, 'Mah', 'Patra', 2145558198, '800 Radha St, Paris, TX');
+
+    INSERT INTO Customer(accountNum, firstName, lastName, phoneNum, billingAddress)
+        Values(9, 'Travis', 'Love', 3336541234, '66 Why Not Dr, Oden, TX');
+
+    INSERT INTO Customer(accountNum, firstName, lastName, phoneNum, billingAddress)
+        Values(10, 'Flash', 'Robin', 8769994356, 871 Nice Blvd, Thor, TX');
+
 */
