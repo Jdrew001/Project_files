4ec00fbfa2640f2c443e04014f3d6d2a8425b7a8
Bobby Henson <hunnant@Bobbys-MacBook-Pro.local>
2017-08-05 13:08:52 -0500

    Employee_load_data

---
+++ Employee_load_data.sql
@@ -0,0 +1,33 @@
+/*
+INSERT INTO Employee(empId, ssn, firstName, lastName, eAdress, phone, salary)
+VALUES(1, '654761234', 'Door', 'Handle', 'frame@gmail.com', '4323218790', '30123)'
+
+INSERT INTO Employee(empId, ssn, firstName, lastName, eAdress, phone, salary)
+VALUES(2, '332111765', 'New', 'Man', 'awomen@gmail.com', '6757863211', '43257');
+
+INSERT INTO Employee(empId, ssn, firstName, lastName, eAdress, phone, salary)
+VALUES(3, '873277754', 'Tissue', 'Paper', 'box@yahool.com', '8179953215', '60321');
+
+INSERT INTO Employee(empId, ssn, firstName, lastName, eAdress, phone, salary)
+VALUES(4, '211456765', 'Swing', 'Set', 'playground@aol.com', '2167079878', '15765');
+
+INSERT INTO Employee(empId, ssn, firstName, lastName, eAdress, phone, salary)
+VALUES(5, '678666453', 'Pro', 'Gram', 'terminal@gmail.com', '8980004561', '28761');
+
+INSERT INTO Employee(empId, ssn, firstName, lastName, eAdress, phone, salary)
+VALUES(6, '173899003', 'Handi', 'Cap', 'Blue@aol.com', '8766666099', '38555');
+
+INSERT INTO Employee(empId, ssn, firstName, lastName, eAdress, phone, salary)
+VALUES(7, '333000333', 'Key', 'Board', 'computer@yahoo.com', '9997771000', '80675');
+
+INSERT INTO Employee(empId, ssn, firstName, lastName, eAdress, phone, salary)
+VALUES(8, '676777890', 'Tea', 'Pot', 'handle@gmail.com', '4118198561', '53556');
+
+INSERT INTO Employee(empId, ssn, firstName, lastName, eAdress, phone, salary)
+VALUES(9, '121343131', 'Chip', 'Bag', 'rack@aol.com', '4415648907', '45678');
+
+INSERT INTO Employee(empId, ssn, firstName, lastName, eAdress, phone, salary)
+VALUES(10, '777888999', 'Cow', 'Boy', 'star@yahoo.com', '6788769981', '77891');
+
+
+*/
