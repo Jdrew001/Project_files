ad11d6ed26e12a3de5da294cb959d5ab04248d98
Gerardo Calvillo <jerry@Gerardos-MacBook-Pro.local>
2017-08-05 12:12:13 -0500

    Updated queries.sql

--- queries.sql
+++ queries.sql
@@ -8,3 +8,11 @@ WHERE cus.accountNum = ord.accountNum
 ;
 
 /*2)*/
+SELECT ord.orderNum, ord.shippingAddress, cus.lastName, cus.firstName, cus.billingAddress,
+cus.accountNum, emp.lastName, emp.firstName
+FROM /////.Orders ord, /////.Customer cus, /////.Employee emp
+WHERE ord.accountNum = cus.accountNum
+AND ord.empId = emp.empId
+;
+
+/*3)*/
