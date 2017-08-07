29e1d67291cf2086ea6c53a0f2cd5fc57b575cab
Bobby Henson <hensonheng@gmail.com>
2017-08-07 16:42:04 -0500

    Driver_load_data.sql

---
+++ Driver_load_data.sql
@@ -0,0 +1,37 @@
+/*
+INSERT INTO Driver(empid, licenseNum, licenseExp, vehicleNum,)
+    Values(1, '11111111', '13-FEB-21', '32341');
+
+INSERT INTO Driver(empid, licenseNum, licenseExp, vehicleNum,)
+    Values(2, '22222222', '12-JAN-20', '54356');
+
+INSERT INTO Driver(empid, licenseNum, licenseExp, vehicleNum,)
+    Values(3, '33333333', '19-DEC-22', '23423');
+
+INSERT INTO Driver(empid, licenseNum, licenseExp, vehicleNum,)
+    Values(4, '44444444', '05-SEPT-19', '98342');
+
+INSERT INTO Driver(empid, licenseNum, licenseExp, vehicleNum,)
+    Values(5, '55555555', '13-JUNE-22', '54353');
+
+INSERT INTO Driver(empid, licenseNum, licenseExp, vehicleNum,)
+    Values(6, '66666666', '01-AUG-18', '32311');
+
+INSERT INTO Driver(empid, licenseNum, licenseExp, vehicleNum,)
+    Values(7, '77777777', '03-FEB-25', '22312');
+
+INSERT INTO Driver(empid, licenseNum, licenseExp, vehicleNum,)
+    Values(8, '88888888', '01-DEC-28', '78776');
+
+INSERT INTO Driver(empid, licenseNum, licenseExp, vehicleNum,)
+    Values(9, '99999999', '101023', '82313');
+
+INSERT INTO Driver(empid, licenseNum, licenseExp, vehicleNum,)
+    Values(10, '10101010', '22-MAY-22', '99223');
+
+
+
+
+
+
+*/
---
+++ Orders_load_data.sql
@@ -0,0 +1,12 @@
+/*
+INSERT INTO Orders(orderNum, shippingAddress, orderDate, empID, accountNum)
+VALUES(1, 'Accounting', 'This is the nerds', 'Business');
+
+
+
+
+
+
+
+
+*/
