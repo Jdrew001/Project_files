9409f846a77453cc41deaa41042c7f098c6531b6
jefferylinnear <30819598+jefferylinnear@users.noreply.github.com>
2017-08-08 11:37:28 -0500

    Sales Rep Load Data
    ...

---
+++ salesRepLoadData.sql
@@ -0,0 +1,11 @@
+insert into Sales Rep(empId, commission, profitGenerated)
+	values(7, 7.56, 20000)
+
+insert into Sales Rep(empId, commission, profitGenerated)
+	values(8, 9.25, 34000)
+
+insert into Sales Rep(empId, commission, profitGenerated)
+	values(9, 8.35, 40000)
+
+insert into Sales Rep(empId, commission, profitGenerated)
+	values(10, 9.75, 50000)
