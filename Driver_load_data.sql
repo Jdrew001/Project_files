c13d48f2dfc667ec95d794f7a1873acb21657823
Drew Atkison <dtatkison@gmail.com>
2017-08-08 11:20:27 -0500

    Fixed driver table

--- Driver_load_data.sql
+++ Driver_load_data.sql
@@ -1,39 +1,3 @@
-<<<<<<< HEAD
-/*
-INSERT INTO Driver(empid, licenseNum, licenseExp, vehicleNum,)
-    Values(1, '11111111', '13-FEB-21', '32341');
-
-INSERT INTO Driver(empid, licenseNum, licenseExp, vehicleNum,)
-    Values(2, '22222222', '12-JAN-20', '54356');
-
-INSERT INTO Driver(empid, licenseNum, licenseExp, vehicleNum,)
-    Values(3, '33333333', '19-DEC-22', '23423');
-
-INSERT INTO Driver(empid, licenseNum, licenseExp, vehicleNum,)
-    Values(4, '44444444', '05-SEP-19', '98342');
-
-INSERT INTO Driver(empid, licenseNum, licenseExp, vehicleNum,)
-    Values(5, '55555555', '13-JUN-22', '54353');
-
-INSERT INTO Driver(empid, licenseNum, licenseExp, vehicleNum,)
-    Values(6, '66666666', '01-AUG-18', '32311');
-
-INSERT INTO Driver(empid, licenseNum, licenseExp, vehicleNum,)
-    Values(7, '77777777', '03-FEB-25', '22312');
-
-INSERT INTO Driver(empid, licenseNum, licenseExp, vehicleNum,)
-    Values(8, '88888888', '01-DEC-28', '78776');
-
-INSERT INTO Driver(empid, licenseNum, licenseExp, vehicleNum,)
-    Values(9, '99999999', '10-OCT-23', '82313');
-
-INSERT INTO Driver(empid, licenseNum, licenseExp, vehicleNum,)
-    Values(10, '10101010', '22-MAY-22', '99223');
-
-
-*/
-=======
-
 INSERT INTO Driver(empid, licenseNum, licenseExp, vehicleNum)
     Values(1, '11111111', '13-FEB-21', [addForeignKeyNum]);
 
@@ -51,6 +15,3 @@ INSERT INTO Driver(empid, licenseNum, licenseExp, vehicleNum)
 
 INSERT INTO Driver(empid, licenseNum, licenseExp, vehicleNum)
     Values(6, '66666666', '01-AUG-18', [addForeignKeyNum]);
-
-
->>>>>>> c5e44a0982c5641afc8447fcb43ddde28857c367
