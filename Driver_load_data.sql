b80c0516f5db357421836a96c96745cef43cb1a8
HUNNANT <30754435+HUNNANT@users.noreply.github.com>
2017-08-07 19:55:05 -0500

    Add files via upload

---
+++ Driver_load_data.sql
@@ -0,0 +1,33 @@
+/*
+INSERT INTO Driver(empid, licenseNum, licenseExp, vehicleNum,)
+    Values(1, '11111111', '13-FEB-21', '32341');
+
+INSERT INTO Driver(empid, licenseNum, licenseExp, vehicleNum,)
+    Values(2, '22222222', '12-JAN-20', '54356');
+
+INSERT INTO Driver(empid, licenseNum, licenseExp, vehicleNum,)
+    Values(3, '33333333', '19-DEC-22', '23423');
+
+INSERT INTO Driver(empid, licenseNum, licenseExp, vehicleNum,)
+    Values(4, '44444444', '05-SEP-19', '98342');
+
+INSERT INTO Driver(empid, licenseNum, licenseExp, vehicleNum,)
+    Values(5, '55555555', '13-JUN-22', '54353');
+
+INSERT INTO Driver(empid, licenseNum, licenseExp, vehicleNum,)
+    Values(6, '66666666', '01-AUG-18', '32311');
+
+INSERT INTO Driver(empid, licenseNum, licenseExp, vehicleNum,)
+    Values(7, '77777777', '03-FEB-25', '22312');
+
+INSERT INTO Driver(empid, licenseNum, licenseExp, vehicleNum,)
+    Values(8, '88888888', '01-DEC-28', '78776');
+
+INSERT INTO Driver(empid, licenseNum, licenseExp, vehicleNum,)
+    Values(9, '99999999', '10-OCT-23', '82313');
+
+INSERT INTO Driver(empid, licenseNum, licenseExp, vehicleNum,)
+    Values(10, '10101010', '22-MAY-22', '99223');
+
+
+*/
