32dae5657a92a0714da1b215ce76cec8ff0b328b
Drew Atkison <dtatkison@gmail.com>
2017-08-08 11:34:38 -0500

    Updated each script to reference a foreign key

--- Driver_load_data.sql
+++ Driver_load_data.sql
@@ -1,17 +1,17 @@
 INSERT INTO Driver(empid, licenseNum, licenseExp, vehicleNum)
-    Values(1, '11111111', '13-FEB-21', [addForeignKeyNum]);
+    Values(1, '11111111', '13-FEB-21', 00002);
 
 INSERT INTO Driver(empid, licenseNum, licenseExp, vehicleNum)
-    Values(2, '22222222', '12-JAN-20', [addForeignKeyNum]);
+    Values(2, '22222222', '12-JAN-20', 00003);
 
 INSERT INTO Driver(empid, licenseNum, licenseExp, vehicleNum)
-    Values(3, '33333333', '19-DEC-22', [addForeignKeyNum]);
+    Values(3, '33333333', '19-DEC-22', 00004);
 
 INSERT INTO Driver(empid, licenseNum, licenseExp, vehicleNum)
-    Values(4, '44444444', '05-SEP-19', [addForeignKeyNum]);
+    Values(4, '44444444', '05-SEP-19', 00005);
 
 INSERT INTO Driver(empid, licenseNum, licenseExp, vehicleNum)
-    Values(5, '55555555', '13-JUN-22', [addForeignKeyNum]);
+    Values(5, '55555555', '13-JUN-22', 00006);
 
 INSERT INTO Driver(empid, licenseNum, licenseExp, vehicleNum)
-    Values(6, '66666666', '01-AUG-18', [addForeignKeyNum]);
+    Values(6, '66666666', '01-AUG-18', 00007);
