a2f3b65ff849e231a33fc0c4a44a7714308eb1e7
calvillo5590 <calvillo5590@gmail.com>
2017-08-07 21:52:52 -0500

    added more shipments

--- shipment_load_data.sql
+++ shipment_load_data.sql
@@ -1,2 +1,29 @@
 INSERT INTO Shipment(shipmentId, shippingDate, orderNum)
-VALUES()
+VALUES(000001, '01-JAN-2018', );
+
+INSERT INTO Shipment(shipmentId, shippingDate, orderNum)
+VALUES(000002, '02-JAN-2018', );
+
+INSERT INTO Shipment(shipmentId, shippingDate, orderNum)
+VALUES(000003, '03-JAN-2018', );
+
+INSERT INTO Shipment(shipmentId, shippingDate, orderNum)
+VALUES(000004, '03-JAN-2018', );
+
+INSERT INTO Shipment(shipmentId, shippingDate, orderNum)
+VALUES(000005, '04-JAN-2018', );
+
+INSERT INTO Shipment(shipmentId, shippingDate, orderNum)
+VALUES(000006, '05-JAN-2018', );
+
+INSERT INTO Shipment(shipmentId, shippingDate, orderNum)
+VALUES(000007, '05-JAN-2018', );
+
+INSERT INTO Shipment(shipmentId, shippingDate, orderNum)
+VALUES(000008, '05-JAN-2018', );
+
+INSERT INTO Shipment(shipmentId, shippingDate, orderNum)
+VALUES(000009, '06-JAN-2018', );
+
+INSERT INTO Shipment(shipmentId, shippingDate, orderNum)
+VALUES(000010, '06-JAN-2018', );
