ce4744826bbb42d64269bafc2b145b94cb8d43e2
Drew Atkison <dtatkison@gmail.com>
2017-08-08 11:57:45 -0500

    Combined all scripts to one

--- salesRepLoadData.sql
+++ salesRepLoadData.sql
@@ -1,11 +1,11 @@
-insert into Sales Rep(empId, commission, profitGenerated)
-	values(7, 7.56, 20000)
+insert into SalesRep(empId, commission, profitGenerated)
+	values(7, 7.56, 20000);
 
-insert into Sales Rep(empId, commission, profitGenerated)
-	values(8, 9.25, 34000)
+insert into SalesRep(empId, commission, profitGenerated)
+	values(8, 9.25, 34000);
 
-insert into Sales Rep(empId, commission, profitGenerated)
-	values(9, 8.35, 40000)
+insert into SalesRep(empId, commission, profitGenerated)
+	values(9, 8.35, 40000);
 
-insert into Sales Rep(empId, commission, profitGenerated)
-	values(10, 9.75, 50000)
+insert into SalesRep(empId, commission, profitGenerated)
+	values(10, 9.75, 50000);
