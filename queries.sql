ab107ce77e6b77ca2aca104c12e36a31ed827536
Gerardo Calvillo <jerry@Gerardos-MacBook-Pro.local>
2017-08-08 12:22:16 -0500

    updated queries script

--- queries.sql
+++ queries.sql
@@ -3,21 +3,21 @@
 
 /*1)*/
 SELECT cus.lastName, cus.firstName, cus.billingAddress, ord.shippingAddress, cus.phoneNum
-FROM /////.Customer cus, /////.Orders ord
+FROM Customer cus, Orders ord
 WHERE cus.accountNum = ord.accountNum
 ;
 
 /*2)*/
 SELECT ord.orderNum, ord.shippingAddress, cus.lastName, cus.firstName, cus.billingAddress,
 cus.accountNum, emp.lastName, emp.firstName
-FROM /////.Orders ord, /////.Customer cus, /////.Employee emp
+FROM Orders ord, Customer cus, Employee emp
 WHERE ord.accountNum = cus.accountNum
-AND ord.empId = emp.empId
+AND ord.empId = emp.empId and ord.orderNum = 1
 ;
 
-SELECT fur.itemCode, fur.description, fur.unitPrice, pur.quantitySold, pur.extendedPrice
-FROM /////.Furniture fur, /////.Purchase pur
-WHERE pur.orderNum = '/////'
+SELECT fur.itemCode, fur.fdescription, fur.unitPrice, pur.quantitySold, pur.quantitySold * fur.unitPrice as extendedPrice
+FROM Furniture fur, Purchase pur
+WHERE pur.orderNum = 1
 AND fur.itemCode = pur.itemCode
 ; 
 
