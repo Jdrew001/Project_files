9299fcd9974aa98aedf82c2591e9bc0037f29891
Drew Atkison <dtatkison@gmail.com>
2017-08-13 20:01:49 -0500

    Project files

--- queries.sql
+++ queries.sql
@@ -4,8 +4,7 @@
 /*1)*/
 SELECT cus.lastName, cus.firstName, cus.billingAddress, ord.shippingAddress, cus.phoneNum
 FROM Customer cus, Orders ord
-WHERE cus.accountNum = ord.accountNum
-;
+WHERE cus.accountNum = ord.accountNum;
 
 /*2)*/
 SELECT ord.orderNum, ord.shippingAddress, cus.lastName, cus.firstName, cus.billingAddress,
